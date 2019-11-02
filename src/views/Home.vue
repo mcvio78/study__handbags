35a902d2412bf5bdd5294754328dc666214dcc47
Mauro <mcvio78@gmail.com>
2019-11-02 21:45:41 +0100

    working with large hero pics.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
 	<div class="home">
 		<h1>Homepage</h1>
+		<div>
+			<v-img :src="heroLarge" :alt="myDescription"></v-img>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	components: {}
+	name: 'Home',
+	data() {
+		return {
+			myDescription: 'An Image of a couple of Handbags.',
+			heroLarge: require('./../client/img/hero/hero_1920x654.jpg')
+		};
+	}
 };
 </script>
