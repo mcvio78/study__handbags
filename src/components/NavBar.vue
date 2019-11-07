acc1625253fb4e4d3296c5aebfecf18541244ec2
Mauro <mcvio78@gmail.com>
2019-11-07 01:07:18 +0100

    optimized photos, responsive home page.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,32 @@
 <template>
-	<nav>
-		<v-app-bar color="purple darken-1" dark>
-			<v-container fluid>
-				<v-row>
-					<v-col cols="3">
-						<v-toolbar-title class="headline">
-							<span class="font-weight-bold">ZH</span>
-							<span class="font-weight-thin grey--text">andbags</span>
-						</v-toolbar-title>
-					</v-col>
+	<v-container fluid class="pa-0">
+		<v-row no-gutters>
+			<v-app-bar color="purple darken-1" dark>
+				<v-col cols="6">
+					<v-toolbar-title class="headline">
+						<span class="font-weight-bold">ZH</span>
+						<span class="font-weight-thin grey--text">andbags</span>
+					</v-toolbar-title>
+				</v-col>
 
-					<v-col cols="6" class="text-center">
-						<v-btn color="deep-purple lighten-3">Patrizia</v-btn>
-						<v-btn color="deep-purple lighten-3">Melania</v-btn>
-						<v-btn color="deep-purple lighten-3">Olivia</v-btn>
-						<v-btn color="deep-purple lighten-3">Anna</v-btn>
-					</v-col>
+				<!--				<v-col cols="8" class="text-sm-center s">-->
+				<!--					<v-btn color="deep-purple lighten-3" class='v-size&#45;&#45;large ma-1'>Patrizia</v-btn>-->
+				<!--					<v-btn color="deep-purple lighten-3" class='v-size&#45;&#45;large ma-1'>Olivia</v-btn>-->
+				<!--					<v-btn color="deep-purple lighten-3" class='v-size&#45;&#45;large ma-1'>Melania</v-btn>-->
+				<!--					<v-btn color="deep-purple lighten-3" class='v-size&#45;&#45;large ma-1'>Anna</v-btn>-->
+				<!--				</v-col>-->
 
-					<v-col cols="3" class="text-right">
-						<v-btn color="deep-purple lighten-3 black--text">
-							<v-icon left>mdi-cart</v-icon>
-							<span>Cart</span>
-						</v-btn>
-					</v-col>
-				</v-row>
-			</v-container>
-		</v-app-bar>
-	</nav>
+				<v-spacer></v-spacer>
+
+				<v-col cols="6" class="text-end">
+					<v-btn color="deep-purple lighten-3 black--text" class="v-size--large">
+						<v-icon left>mdi-cart</v-icon>
+						<span>Cart</span>
+					</v-btn>
+				</v-col>
+			</v-app-bar>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,62 +2,41 @@
 	<v-container fluid class="fill-height pa-0 purple lighten-5">
 		<!--<h1>Homepage</h1>-->
 		<v-row>
-			<v-col>
-				<!--				<img src="cat-200px.jpg" alt="sugar free cat" width="200"-->
-				<!--				     srcset="cat-200px.jpg 1x, -->
-				<!--             cat-400px.jpg 2x, -->
-				<!--             cat-600px.jpg 3x,-->
-				<!--             cat-800px.jpg 4x">-->
-
-				<!--				<img src="./../client/img/hero/heroFallback_640x654.jpg" :alt="myDescription" width="100%"-->
-				<!--				     srcset="./../client/img/hero/heroFallback_640x654.jpg 1x,-->
-				<!--             ./../client/img/hero/hero_990x654.jpg 2x,-->
-				<!--             ./../client/img/hero/hero_1380x654.jpg 3x,-->
-				<!--             ./../client/img/hero/hero_1920x654.jpg 4x">-->
-
+			<v-col class="text-center" cols="12">
 				<picture>
 					<source
-						srcset="
-						./../client/img/hero/hero_1920x654.jpg 1920w 1x,
-						./../client/img/hero/hero_3840x1308.jpg 3840 2x"
+						type="image/jpg"
 						media="(min-width: 1380px)"
+						srcset="
+							./../client/img/hero-optimized/hero_large.jpg         1920w,
+							./../client/img/hero-optimized/hero_large--hi-dpi.jpg 3840w
+						"
 					/>
 					<source
-						srcset="
-						./../client/img/hero/hero_1380x654.jpg 1380w 1x,
-						./../client/img/hero/hero_2760x1308.jpg 2760 2x"
+						type="image/jpg"
 						media="(min-width: 990px)"
+						srcset="
+							./../client/img/hero-optimized/hero_medium.jpg         1380w,
+							./../client/img/hero-optimized/hero_medium--hi-dpi.jpg 2760w
+						"
 					/>
 					<source
-						srcset="
-						./../client/img/hero/hero_990x654.jpg 990w 1x,
-						./../client/img/hero/hero_1980x1308.jpg 1980w 2x"
+						type="image/jpg"
 						media="(min-width: 640px)"
+						srcset="
+							./../client/img/hero-optimized/hero_small.jpg          990w,
+							./../client/img/hero-optimized/hero_small--hi-dpi.jpg 1980w
+						"
 					/>
 					<source
+						type="image/jpg"
 						srcset="
-						./../client/img/hero/hero_640x654.jpg 640w 1x,
-						./../client/img/hero/hero_1280x1308.jpg 1280w 2x"
+							./../client/img/hero-optimized/hero_smaller.jpg          640w,
+							./../client/img/hero-optimized/hero_smaller--hi-dpi.jpg 1280w
+						"
 					/>
-					<img src="./../client/img/hero/heroFallback_640x654.jpg" :alt="myDescription" />
+					<img src="./../client/img/hero-optimized/hero_smaller--fallback.jpg" :alt="myDescription" />
 				</picture>
-
-				<!--				<v-img-->
-				<!--					sizes="-->
-				<!--					(min-width: 1380px) 1920w,-->
-				<!--            (min-width: 990px) 1380w,-->
-				<!--				     (min-width: 640px) 990w"-->
-				<!--					:srcset="-->
-				<!--						`-->
-				<!--					${heroLarge} 1920w,-->
-				<!--					${heroMedium} 1380w,-->
-				<!--					${heroSmall} 990w,-->
-				<!--					${heroSmaller} 640w-->
-				<!--					`-->
-				<!--					"-->
-				<!--					:src="`${heroSmaller}`"-->
-				<!--					:alt="myDescription"-->
-				<!--				></v-img>-->
 			</v-col>
 		</v-row>
 	</v-container>
@@ -68,18 +47,18 @@ export default {
 	name: 'Home',
 	data() {
 		return {
-			// heroLarge: require('./../client/img/hero/hero_1920x654.jpg'),
-			// heroLargeHiDpi: require('./../client/img/hero/hero_3840x1308.jpg'),
+			// heroLarge: require('./../client/img/hero-optimized/hero_large.jpg'),
+			// heroLargeHiDpi: require('./../client/img/hero-optimized/hero_smaller--hi-dpi.jpg'),
 			//
-			// heroMedium: require('./../client/img/hero/hero_1380x654.jpg'),
-			// heroMediumHiDpi: require('./../client/img/hero/hero_2760x1308.jpg'),
+			// heroMedium: './../client/img/hero-optimized/hero_medium.jpg',
+			// heroMediumHiDpi: require('./../client/img/hero-optimized/hero_medium--hi-dpi.jpg'),
 			//
-			// heroSmall: require('./../client/img/hero/hero_990x654.jpg'),
-			// heroSmallHiDpi: require('./../client/img/hero/hero_1980x1308.jpg'),
+			// heroSmall: require('./../client/img/hero-optimized/hero_small.jpg'),
+			// heroSmallHiDpi: require('./../client/img/hero-optimized/hero_small.jpg'),
 			//
-			// heroSmaller: require('./../client/img/hero/heroFallback_640x654.jpg'),
-			// heroSmallerHiDpi: require('./../client/img/hero/hero_1280x1308.jpg'),
-
+			// heroSmaller: require('./../client/img/hero-optimized/hero_smaller.jpg'),
+			// heroSmallerHiDpi: require('./../client/img/hero-optimized/hero_smaller.jpg'),
+			//
 			myDescription: 'An Image of a couple of Handbags.'
 		};
 	}
