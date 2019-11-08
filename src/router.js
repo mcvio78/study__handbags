88d53c83aebf8aad89dfd773ad7096d39aad68c3
Mauro <mcvio78@gmail.com>
2019-11-08 23:20:37 +0100

    create list router path with props: true, create list.vue view.

--- src/router.js
+++ src/router.js
@@ -15,12 +15,13 @@ export default new Router({
 			component: Home
 		},
 		{
-			path: '/about',
-			name: 'about',
+			path: '/list/:bagType',
+			name: 'list',
+			props: true,
 			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
+			// this generates a separate chunk (list.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+			component: () => import(/* webpackChunkName: "list" */ './views/List.vue')
 		},
 		{ path: '*', component: Error_404 }
 	]
---
+++ src/views/List.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="list">
+		<h1>This is a list page</h1>
+		<h2>{{ bagType }}</h2>
+		<h2>{{ borse }}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'list',
+		props: ['bagType'],
+		computed: {
+			borse() {
+				 return this.$store.state.handbags.series
+			}
+		}
+	}
+</script>
