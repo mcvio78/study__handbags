644e5e2aee7a7871aeb273cdda1839868efec961
Mauro <mcvio78@gmail.com>
2019-11-09 22:26:55 +0100

    replace the photo paths with computed properties.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,44 @@
 <template>
 	<v-container fluid class="py-0 purple lighten-5" height="100%">
-		<!--<h1>Homepage</h1>-->
 		<v-row>
 			<v-col class="text-center pa-0" cols="12">
 				<picture>
 					<source
 						type="image/jpg"
 						media="(min-width: 1380px)"
-						srcset="
-							./../client/img/hero-optimized/hero_large.jpg         1920w,
-							./../client/img/hero-optimized/hero_large--hi-dpi.jpg 3840w
-						"
+						:srcset="heroLarge + ' 1920w, ' + heroLargeHiDpi + ' 3840w'"
 					/>
+
 					<source
 						type="image/jpg"
 						media="(min-width: 990px)"
-						srcset="
-							./../client/img/hero-optimized/hero_medium.jpg         1380w,
-							./../client/img/hero-optimized/hero_medium--hi-dpi.jpg 2760w
-						"
+						:srcset="heroMedium + ' 1380w, ' + heroMediumHiDpi + ' 2760w'"
 					/>
+
 					<source
 						type="image/jpg"
 						media="(min-width: 640px)"
-						srcset="
-							./../client/img/hero-optimized/hero_small.jpg          990w,
-							./../client/img/hero-optimized/hero_small--hi-dpi.jpg 1980w
-						"
+						:srcset="heroSmall + ' 990w, ' + heroSmallHiDpi + ' 1980w'"
 					/>
+
 					<source
 						type="image/jpg"
 						media="(min-width: 530px)"
-						srcset="
-							./../client/img/hero-optimized/hero_smaller.jpg          640w,
-							./../client/img/hero-optimized/hero_smaller--hi-dpi.jpg 1280w
-						"
+						:srcset="heroSmaller + ' 640w, ' + heroSmallerHiDpi + ' 1280w'"
 					/>
-					<source
+
+					<source type="image/jpg" :srcset="heroSmallest + ' 530w, ' + heroSmallestHiDpi + ' 1060w'" />
+
+					<img
 						type="image/jpg"
-						srcset="
-							./../client/img/hero-optimized/hero_smallest.jpg          530w,
-							./../client/img/hero-optimized/hero_smallest--hi-dpi.jpg 1060w
-						"
+						:src="heroSmallestFallback"
+						:srcset="heroSmall + ' 990w, ' + heroLarge + ' 1920w'"
+						:alt="myDescription"
 					/>
-					<img src="./../client/img/hero-optimized/hero_smallest--fallback.jpg" :alt="myDescription" />
 				</picture>
 			</v-col>
 		</v-row>
+
 		<v-row class="justify-center">
 			<v-col cols="11" class="col-md-10">
 				<h1 class="display-1 mb-2">Philosophy:</h1>
@@ -89,20 +81,43 @@ export default {
 	data() {
 		// TODO Complete responsive photos with relative paths.
 		return {
-			// heroLarge: require('./../client/img/hero-optimized/hero_large.jpg'),
-			// heroLargeHiDpi: require('./../client/img/hero-optimized/hero_smaller--hi-dpi.jpg'),
-			//
-			// heroMedium: './../client/img/hero-optimized/hero_medium.jpg',
-			// heroMediumHiDpi: require('./../client/img/hero-optimized/hero_medium--hi-dpi.jpg'),
-			//
-			// heroSmall: require('./../client/img/hero-optimized/hero_small.jpg'),
-			// heroSmallHiDpi: require('./../client/img/hero-optimized/hero_small.jpg'),
-			//
-			// heroSmaller: require('./../client/img/hero-optimized/hero_smaller.jpg'),
-			// heroSmallerHiDpi: require('./../client/img/hero-optimized/hero_smaller.jpg'),
-			//
 			myDescription: 'An Image of a couple of Handbags.'
 		};
+	},
+	computed: {
+		heroLarge() {
+			return require('./../client/img/hero-optimized/hero_large.jpg');
+		},
+		heroLargeHiDpi() {
+			return require('./../client/img/hero-optimized/hero_large--hi-dpi.jpg');
+		},
+		heroMedium() {
+			return require('./../client/img/hero-optimized/hero_medium.jpg');
+		},
+		heroMediumHiDpi() {
+			return require('./../client/img/hero-optimized/hero_medium--hi-dpi.jpg');
+		},
+		heroSmall() {
+			return require('./../client/img/hero-optimized/hero_small.jpg');
+		},
+		heroSmallHiDpi() {
+			return require('./../client/img/hero-optimized/hero_small--hi-dpi.jpg');
+		},
+		heroSmaller() {
+			return require('./../client/img/hero-optimized/hero_smaller.jpg');
+		},
+		heroSmallerHiDpi() {
+			return require('./../client/img/hero-optimized/hero_smaller--hi-dpi.jpg');
+		},
+		heroSmallest() {
+			return require('./../client/img/hero-optimized/hero_smallest.jpg');
+		},
+		heroSmallestHiDpi() {
+			return require('./../client/img/hero-optimized/hero_smallest--hi-dpi.jpg');
+		},
+		heroSmallestFallback() {
+			return require('./../client/img/hero-optimized/hero_smallest--fallback.jpg');
+		}
 	}
 };
 </script>
