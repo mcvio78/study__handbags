a73ff9ba7937003a28b791f70e7b3875b8d39cbc
Mauro <mcvio78@gmail.com>
2019-11-12 18:45:02 +0100

    add to store links with high and low preview to all handbags.

--- src/store.js
+++ src/store.js
@@ -11,79 +11,79 @@ export default new Vuex.Store({
 					id: 'p01',
 					name: 'Patrizia London',
 					price: 40,
-					imagePreviewLo: require('./../server/img/patrizia_01/patrizia-london-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_01/patrizia-london-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_01/patrizia-london-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_01/patrizia-london-preview--hi-dpi.jpg'),
 					quantity: 3
 				},
 				patrizia_02: {
 					id: 'p02',
 					name: 'Patrizia Dots',
 					price: 40,
-					imagePreviewLo: require('./../server/img/patrizia_02/patrizia-dots-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_02/patrizia-dots-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_02/patrizia-dots-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_02/patrizia-dots-preview--hi-dpi.jpg'),
 					quantity: 1
 				},
 				patrizia_03: {
 					id: 'p03',
 					name: 'Patrizia Flowers',
-					imagePreviewLo: require('./../server/img/patrizia_03/patrizia-flowers-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_03/patrizia-flowers-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_03/patrizia-flowers-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_03/patrizia-flowers-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 5
 				},
 				patrizia_04: {
 					id: 'p04',
 					name: 'Patrizia Painting',
-					imagePreviewLo: require('./../server/img/patrizia_04/patrizia-painting-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_04/patrizia-painting-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_04/patrizia-painting-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_04/patrizia-painting-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 7
 				},
 				patrizia_05: {
 					id: 'p05',
 					name: 'Patrizia Art',
-					imagePreviewLo: require('./../server/img/patrizia_05/patrizia-art-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_05/patrizia-art-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_05/patrizia-art-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_05/patrizia-art-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 11
 				},
 				patrizia_06: {
 					id: 'p06',
 					name: 'Patrizia Aqua',
-					imagePreviewLo: require('./../server/img/patrizia_06/patrizia-aqua-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_06/patrizia-aqua-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_06/patrizia-aqua-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_06/patrizia-aqua-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 2
 				},
 				patrizia_07: {
 					id: 'p07',
 					name: 'Patrizia Venice',
-					imagePreviewLo: require('./../server/img/patrizia_07/patrizia-venice-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_07/patrizia-venice-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_07/patrizia-venice-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_07/patrizia-venice-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 8
 				},
 				patrizia_08: {
 					id: 'p08',
 					name: 'Patrizia U.S.',
-					imagePreviewLo: require('./../server/img/patrizia_08/patrizia-us-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_08/patrizia-us-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_08/patrizia-us-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_08/patrizia-us-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 9
 				},
 				patrizia_09: {
 					id: 'p09',
 					name: 'Patrizia Cusco',
-					imagePreviewLo: require('./../server/img/patrizia_09/patrizia-cusco-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_09/patrizia-cusco-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_09/patrizia-cusco-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_09/patrizia-cusco-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 14
 				},
 				patrizia_10: {
 					id: 'p10',
 					name: 'Patrizia Mandala',
-					imagePreviewLo: require('./../server/img/patrizia_10/patrizia-mandala-preview--lo-dpi.jpg'),
-					imagePreviewHi: require('./../server/img/patrizia_10/patrizia-mandala-preview--hi-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia/patrizia_10/patrizia-mandala-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia/patrizia_10/patrizia-mandala-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 5
 				}
@@ -91,36 +91,50 @@ export default new Vuex.Store({
 			olivia: {
 				olivia_1: {
 					name: 'Olivia Owl',
+					imagePreviewLo: require('./../server/img/olivia/olivia_01/olivia-owl-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/olivia/olivia_01/olivia-owl-preview--hi-dpi.jpg'),
 					price: 35,
 					quantity: 6
 				},
 				olivia_2: {
 					name: 'Olivia Love',
+					imagePreviewLo: require('./../server/img/olivia/olivia_02/olivia-love-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/olivia/olivia_02/olivia-love-preview--hi-dpi.jpg'),
 					price: 35,
 					quantity: 8
 				},
 				olivia_3: {
 					name: 'Olivia Crane',
+					imagePreviewLo: require('./../server/img/olivia/olivia_03/olivia-crane-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/olivia/olivia_03/olivia-crane-preview--hi-dpi.jpg'),
 					price: 35,
 					quantity: 11
 				},
 				olivia_4: {
 					name: 'Olivia Marilyn',
+					imagePreviewLo: require('./../server/img/olivia/olivia_04/olivia-marilyn-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/olivia/olivia_04/olivia-marilyn-preview--hi-dpi.jpg'),
 					price: 35,
 					quantity: 1
 				},
 				olivia_5: {
 					name: 'Olivia Cinquecento',
+					imagePreviewLo: require('./../server/img/olivia/olivia_05/olivia-cinquecento-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/olivia/olivia_05/olivia-cinquecento-preview--hi-dpi.jpg'),
 					price: 35,
 					quantity: 9
 				},
 				olivia_6: {
 					name: 'Olivia B&W',
+					imagePreviewLo: require('./../server/img/olivia/olivia_06/olivia-bw-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/olivia/olivia_06/olivia-bw-preview--hi-dpi.jpg'),
 					price: 35,
 					quantity: 6
 				},
 				olivia_7: {
 					name: 'Olivia Route 66',
+					imagePreviewLo: require('./../server/img/olivia/olivia_07/olivia-route66-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/olivia/olivia_07/olivia-route66-preview--hi-dpi.jpg'),
 					price: 35,
 					quantity: 7
 				}
@@ -128,26 +142,36 @@ export default new Vuex.Store({
 			melania: {
 				melania_1: {
 					name: 'Melania Military',
+					imagePreviewLo: require('./../server/img/melania/melania_01/melania-military-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/melania/melania_01/melania-military-preview--hi-dpi.jpg'),
 					price: 65,
 					quantity: 12
 				},
 				melania_2: {
 					name: 'Melania Pop Art',
+					imagePreviewLo: require('./../server/img/melania/melania_02/melania-pop-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/melania/melania_02/melania-pop-preview--hi-dpi.jpg'),
 					price: 65,
 					quantity: 4
 				},
 				melania_3: {
 					name: 'Melania Sneakers',
+					imagePreviewLo: require('./../server/img/melania/melania_03/melania-sneakers--preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/melania/melania_03/melania-sneakers--preview--hi-dpi.jpg'),
 					price: 65,
 					quantity: 5
 				},
 				melania_4: {
 					name: 'Melania Ballerina',
+					imagePreviewLo: require('./../server/img/melania/melania_04/melania-ballerina-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/melania/melania_04/melania-ballerina-preview--hi-dpi.jpg'),
 					price: 65,
 					quantity: 3
 				},
 				melania_5: {
 					name: 'Melania Vogue',
+					imagePreviewLo: require('./../server/img/melania/melania_05/melania-vogue-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/melania/melania_05/melania-vogue-preview--hi-dpi.jpg'),
 					price: 65,
 					quantity: 11
 				}
@@ -155,51 +179,71 @@ export default new Vuex.Store({
 			anna: {
 				anna_1: {
 					name: 'Anna Crane',
+					imagePreviewLo: require('./../server/img/anna/anna_01/anna-crane-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_01/anna-crane-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 9
 				},
 				anna_2: {
 					name: 'Anna Cats',
+					imagePreviewLo: require('./../server/img/anna/anna_02/anna-cats-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_02/anna-cats-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 11
 				},
 				anna_3: {
 					name: 'Anna Piano',
+					imagePreviewLo: require('./../server/img/anna/anna_03/anna-piano-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_03/anna-piano-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 7
 				},
 				anna_4: {
-					name: 'PatriziAnna Sneakers',
+					name: 'Anna Sneakers',
+					imagePreviewLo: require('./../server/img/anna/anna_04/anna-sneakers-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_04/anna-sneakers-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 2
 				},
 				anna_5: {
 					name: 'Anna B&W',
+					imagePreviewLo: require('./../server/img/anna/anna_05/anna-bw-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_05/anna-bw-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 3
 				},
 				anna_6: {
-					name: 'Annaa Pink Flowers',
+					name: 'Anna Pink Flowers',
+					imagePreviewLo: require('./../server/img/anna/anna_06/anna-pinkflowers-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_06/anna-pinkflowers-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 6
 				},
 				anna_7: {
 					name: 'Anna Blue Flowers',
+					imagePreviewLo: require('./../server/img/anna/anna_07/anna-blueflowers-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_07/anna-blueflowers-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 1
 				},
 				anna_8: {
 					name: 'Anna Route 66',
+					imagePreviewLo: require('./../server/img/anna/anna_08/anna-route-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_08/anna-route-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 11
 				},
 				anna_9: {
 					name: 'Anna Black Flowers',
+					imagePreviewLo: require('./../server/img/anna/anna_09/anna-blackflowers-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_09/anna-blackflowers-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 2
 				},
 				anna_10: {
 					name: 'Anna Black Cat',
+					imagePreviewLo: require('./../server/img/anna/anna_10/anna-blackcat-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/anna/anna_10/anna-blackcat-preview--hi-dpi.jpg'),
 					price: 25,
 					quantity: 12
 				}
