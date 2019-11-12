6cffccb0fe6b85baee8e776a54b712787130004b
Mauro <mcvio78@gmail.com>
2019-11-12 12:18:46 +0100

    BagsList now iterates on bagType selected and passed as a prop.

--- src/views/BagsList.vue
+++ src/views/BagsList.vue
@@ -1,15 +1,29 @@
 <template>
-	<div class="list">
+	<div class="bagsList">
 		<h1>This is the BagList page</h1>
-		<h2>This is the prop that NavBar has passed: {{ bagType }}</h2>
+		<h2>This is the prop that NavBar has passed: - {{ bagType }} -</h2>
 
-		<BaseBagCard></BaseBagCard>
+		<v-container fluid>
+			<v-row>
+				<v-col class="text-center">
+					<BaseBagCard v-for="handbag in handbags[bagType]" :key="handbag.id" :handbag="handbag"></BaseBagCard>
+				</v-col>
+			</v-row>
+		</v-container>
 	</div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
 	name: 'BagsList',
-	props: ['bagType']
+	props: {
+		bagType: {
+			type: String,
+			required: true
+		}
+	},
+	computed: mapState(['handbags'])
 };
 </script>
