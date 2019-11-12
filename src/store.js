8deff2580fa0519c32bbeb54e1f38dd6abfaa306
Mauro <mcvio78@gmail.com>
2019-11-12 12:13:35 +0100

    add to database requires to low and high resolution preview.

--- src/store.js
+++ src/store.js
@@ -11,60 +11,79 @@ export default new Vuex.Store({
 					id: 'p01',
 					name: 'Patrizia London',
 					price: 40,
-					image: require('./../server/img/patrizia_01/patrizia-london--preview-lo-dpi.jpg'),
+					imagePreviewLo: require('./../server/img/patrizia_01/patrizia-london-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_01/patrizia-london-preview--hi-dpi.jpg'),
 					quantity: 3
 				},
 				patrizia_02: {
 					id: 'p02',
 					name: 'Patrizia Dots',
 					price: 40,
+					imagePreviewLo: require('./../server/img/patrizia_02/patrizia-dots-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_02/patrizia-dots-preview--hi-dpi.jpg'),
 					quantity: 1
 				},
 				patrizia_03: {
 					id: 'p03',
 					name: 'Patrizia Flowers',
+					imagePreviewLo: require('./../server/img/patrizia_03/patrizia-flowers-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_03/patrizia-flowers-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 5
 				},
 				patrizia_04: {
 					id: 'p04',
 					name: 'Patrizia Painting',
+					imagePreviewLo: require('./../server/img/patrizia_04/patrizia-painting-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_04/patrizia-painting-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 7
 				},
 				patrizia_05: {
 					id: 'p05',
 					name: 'Patrizia Art',
+					imagePreviewLo: require('./../server/img/patrizia_05/patrizia-art-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_05/patrizia-art-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 11
 				},
 				patrizia_06: {
 					id: 'p06',
 					name: 'Patrizia Aqua',
+					imagePreviewLo: require('./../server/img/patrizia_06/patrizia-aqua-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_06/patrizia-aqua-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 2
 				},
 				patrizia_07: {
 					id: 'p07',
 					name: 'Patrizia Venice',
+					imagePreviewLo: require('./../server/img/patrizia_07/patrizia-venice-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_07/patrizia-venice-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 8
 				},
 				patrizia_08: {
 					id: 'p08',
 					name: 'Patrizia U.S.',
+					imagePreviewLo: require('./../server/img/patrizia_08/patrizia-us-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_08/patrizia-us-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 9
 				},
 				patrizia_09: {
 					id: 'p09',
-					name: 'Patrizia Cuzco',
+					name: 'Patrizia Cusco',
+					imagePreviewLo: require('./../server/img/patrizia_09/patrizia-cusco-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_09/patrizia-cusco-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 14
 				},
 				patrizia_10: {
 					id: 'p10',
 					name: 'Patrizia Mandala',
+					imagePreviewLo: require('./../server/img/patrizia_10/patrizia-mandala-preview--lo-dpi.jpg'),
+					imagePreviewHi: require('./../server/img/patrizia_10/patrizia-mandala-preview--hi-dpi.jpg'),
 					price: 40,
 					quantity: 5
 				}
