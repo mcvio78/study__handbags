794fa5be021801f6b12caf17c1c18cde124a0e9e
Mauro <mcvio78@gmail.com>
2019-11-22 19:52:26 +0100

    create first action fetching events with axios, create first mutation putting all data on store.

--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,26 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+import HandbagsService from './../services/HandbagsService';
+
 export default new Vuex.Store({
 	state: {
-		mutations: {},
-		actions: {}
+		handbags: {}
+	},
+	mutations: {
+		SET_EVENTS(state, names) {
+			state.handbags = names;
+		}
+	},
+	actions: {
+		fetchEvents({ commit }) {
+			HandbagsService.getHandbagsService()
+				.then(response => {
+					commit('SET_EVENTS', response.data);
+				})
+				.catch(error => {
+					console.log('There was a problem fetching events: ', error.message);
+				});
+		}
 	}
 });
