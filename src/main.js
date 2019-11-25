582c7dc7c78cff1d76c6d3b5813d9821d749db2d
Mauro <mcvio78@gmail.com>
2019-11-26 00:47:23 +0100

    import only firebase authentication, initialize firebase with process.env.VUE_APP

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store/store';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 Vue.config.productionTip = false;
 
@@ -16,3 +18,16 @@ new Vue({
 	vuetify,
 	render: h => h(App)
 }).$mount('#app');
+
+const firebaseConfig = {
+	apiKey: process.env.VUE_APP_API_KEY,
+	authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+	databaseURL: process.env.VUE_APP_DATABASE_URL,
+	projectId: process.env.VUE_APP_PROJECT_ID,
+	storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+	appId: process.env.VUE_APP_ID,
+	measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
