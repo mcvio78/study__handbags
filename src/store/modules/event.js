e686d85953c872444009e4f7a23d131c963435d4
Mauro <mcvio78@gmail.com>
2019-11-25 01:55:00 +0100

    get this module namespaced, remove store hard code objects, when fetch data check if they already exist, create getter to help.

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -1,35 +1,46 @@
 import HandbagsService from '../../services/HandbagsService';
 
+export const namespaced = true;
+
 export const state = {
 	handbags: {
-		collections: {},
-		patrizia: {},
-		olivia: {},
-		melania: {},
-		anna: {}
+		//collections: {},
+		// patrizia: {},
+		// olivia: {},
+		// melania: {},
+		// anna: {}
 	}
 };
 
 export const mutations = {
 	SET_HANDBAGS(state, payload) {
-		state.handbags[payload.subField] = payload.responseData;
+		// state.handbags[payload.subField] = payload.responseData;
+		state.handbags = { ...state.handbags, [payload.subField]: payload.responseData };
 	}
 };
 
 export const actions = {
-	fetchHandbags({ rootState, commit }, subField) {
-		HandbagsService.getHandbagService(subField)
-			.then(response => {
-				if (rootState.user.user.name === 'Mauro') {
-					const payload = {
-						subField: subField,
-						responseData: response.data
-					};
-					commit('SET_HANDBAGS', payload);
-				}
-			})
-			.catch(error => {
-				console.log('There was a problem fetching events: ', error.message);
-			});
+	fetchHandbags({ rootState, commit, getters }, subField) {
+		if (!getters.checkIfCollectionExists(subField)) {
+			HandbagsService.getHandbagService(subField)
+				.then(response => {
+					if (rootState.user.user.name === 'Mauro') {
+						const payload = {
+							subField: subField,
+							responseData: response.data
+						};
+						commit('SET_HANDBAGS', payload);
+					}
+				})
+				.catch(error => {
+					console.log('There was a problem fetching events: ', error.message);
+				});
+		}
+	}
+};
+
+export const getters = {
+	checkIfCollectionExists: state => subField => {
+		return state.handbags[subField];
 	}
 };
