a0380d89ae2f003abe7b8aad51fe83da2b8dfa69
Mauro <mcvio78@gmail.com>
2019-11-26 15:54:02 +0100

    create notification module with notification array, PUSH and DELETE mutations, add and remove actions, import it on store.

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import * as user from './modules/user';
 import * as event from './modules/event';
+import * as notification from './modules/notification';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	modules: {
 		user,
-		event
+		event,
+		notification
 	},
 	state: {}
 });
