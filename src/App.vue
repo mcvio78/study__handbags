1f9f21916be7f0f7c0345d156a0d2aa05aff4ba5
Mauro <mcvio78@gmail.com>
2019-12-05 18:09:22 +0100

    import and add NotificationBox and ProgressBar as components.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
 	<v-app>
 		<NavBar />
+		<ProgressBar />
+		<notification-box />
 		<v-content>
-			<router-view :key="$route.fullPath"></router-view>
+			<router-view :key="$route.fullPath" />
 		</v-content>
 	</v-app>
 </template>
 
 <script>
 import NavBar from './components/NavBar';
+import NotificationBox from './components/NorificationBox';
+import ProgressBar from './components/ProgressBar';
 
 export default {
 	name: 'App',
-	components: { NavBar },
+	components: { NavBar, NotificationBox, ProgressBar },
 	data() {
 		return {};
 	}
