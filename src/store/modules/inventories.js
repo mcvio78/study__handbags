ec96feada30e21d39f473c1983bd14ef656eaf1f
Mauro <mcvio78@gmail.com>
2020-01-25 22:44:07 +0100

    create updateInventories action to update inventories in database, create method to update cart in Vuex store.

--- src/store/modules/inventories.js
+++ src/store/modules/inventories.js
@@ -1,5 +1,5 @@
-//import firebase from 'firebase/app';
-import HandbagsService from './../../services/HandbagsService';
+import firebase from 'firebase/app';
+import handbagsService from './../../services/HandbagsService';
 
 export const namespaced = true;
 
@@ -18,13 +18,18 @@ export const mutations = {
 	},
 	SET_INVENTORIES_ERROR(state, payload) {
 		state.inventoriesError = payload;
+	},
+	UPDATE_INVENTORIES_FIELD(state, payload) {
+		Object.keys(payload).forEach(key => state.inventories[key] = payload[key]);
+		//state.inventories[payload.itemNumber]['quantity'] = payload.value.quantity;
 	}
 };
 
 export const actions = {
 	/////////////////////////////////////////////////////////////////////////////////////////////////GET QUANTITY DATABASE
 	getInventories({ commit, dispatch }, subField) {
-		return HandbagsService.getHandbagService(subField)
+		return handbagsService
+			.getHandbagService(subField)
 			.then(response => {
 				commit('SET_INVENTORIES', response.data);
 				commit('SET_INVENTORIES_STATUS', 'success');
@@ -42,6 +47,57 @@ export const actions = {
 				};
 				dispatch('notification/add', notification, { root: true });
 			});
+	},
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////UPDATE CART
+	updateInventories({ commit, rootState, dispatch }, payload) {
+		console.log('payload: ', payload);
+		if (rootState.user.user) {
+			return new Promise(resolve => {
+				if (firebase.auth().currentUser) {
+					commit('SET_INVENTORIES_STATUS', 'loading');
+					firebase
+						.auth()
+						.currentUser.getIdToken(/* forceRefresh */ true)
+						.then(idToken =>
+							handbagsService.updateInventoriesService(idToken, firebase.auth().currentUser.uid, payload)
+						)
+						.then(response => {
+							commit('UPDATE_INVENTORIES_FIELD', payload);
+							commit('SET_INVENTORIES_STATUS', 'success');
+							commit('SET_INVENTORIES_ERROR', null);
+							resolve(response.status);
+							dispatch('cart/cleanCart', 'cart', { root: true });
+						})
+						.then(() => {
+							const notification = {
+								type: 'success',
+								field: 'database',
+								name: firebase.auth().currentUser.displayName,
+								message: 'Your order has been sent.'
+							};
+							dispatch('notification/add', notification, { root: true });
+						})
+						.catch(error => {
+							commit('SET_INVENTORIES_STATUS', 'failure');
+							commit('SET_INVENTORIES_ERROR', error.message);
+
+							const notification = {
+								type: 'error',
+								field: 'database',
+								message: `'There was a problem sent your order: '${error.message}`
+							};
+							dispatch('notification/add', notification, { root: true });
+						});
+				} else {
+					//Todo complete behaviour #1.
+					alert('there is no firebase user.');
+				}
+			});
+		} else {
+			//Todo complete behaviour #2.
+			alert('there is no local user data.');
+		}
 	}
 };
 
